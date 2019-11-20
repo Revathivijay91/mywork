00b2f747aec06918c34665fad08d38785bbf482d
revathivijay91 <connectme2reva@gmail.com>
2019-11-20 14:20:14 +0530

    Updates

---
+++ precache-manifest.7c41451bf1e5bf92c186914202053857.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af946aa10bc529712258f51b9064232c",
+    "url": "/mywork/index.html"
+  },
+  {
+    "revision": "e3d5412bdaccfd550e12",
+    "url": "/mywork/static/css/main.fc29d31f.chunk.css"
+  },
+  {
+    "revision": "b7f86af0e357935f6a98",
+    "url": "/mywork/static/js/2.9a0aad50.chunk.js"
+  },
+  {
+    "revision": "e3d5412bdaccfd550e12",
+    "url": "/mywork/static/js/main.fbb37326.chunk.js"
+  },
+  {
+    "revision": "2f7a4eb82c6b2d20210b",
+    "url": "/mywork/static/js/runtime-main.9ebc8897.js"
+  }
+]);
